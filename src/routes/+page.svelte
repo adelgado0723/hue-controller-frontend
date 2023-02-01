8025f04ff5aeebc31f894c95fbf12751cada050a
adelgado <adelgado0723@gmail.com>
2023-02-01 00:34:14 -0500

    adds prettier-plugin-tailwindcss for autosorting classnames

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,13 @@
 	/* const bgURL = new URL('../..//static/controlPanel.png', import.meta.url).href; */
 </script>
 
-<div class="hero min-h-screen" style="background-image: url('./controlPanel.png');">
+<div class=" hero min-h-screen" style="background-image: url('./controlPanel.png');">
 	<div class="hero-overlay bg-opacity-60" />
 	<div class="hero-content text-center text-neutral-content">
 		<div class="max-w-md text-white">
 			<h1 class="mb-5 text-5xl font-bold">Welcome to the hub...</h1>
-			<p class="mb-5">
-      Fine grain control of hue smart home devices at your fingertips.
-			</p>
-			<button class="btn btn-primary">Get Started</button>
+			<p class="mb-5">Fine grain control of hue smart home devices at your fingertips.</p>
+			<button class="btn-primary btn">Get Started</button>
 		</div>
 	</div>
 </div>
