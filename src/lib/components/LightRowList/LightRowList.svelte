717100ca84c93adb85a684106c3a33919cc1a070
adelgado <adelgado0723@gmail.com>
2023-02-24 00:59:00 -0500

    adds light row and light row list

---
+++ src/lib/components/LightRowList/LightRowList.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+	import LightRow from '$lib/components/LightRow/LightRow.svelte';
+	import type { Light as ILight } from '$lib/components/Light/Light';
+
+	import type { Light } from '../Light/Light';
+	import type RequestInit from 'http';
+	/* import LightModal from '$lib/components/Modals/LightModal/LightModal.svelte'; */
+	import { PUBLIC_BRIDGE_IP, PUBLIC_BRIDGE_USERNAME } from '$env/static/public';
+	import type { UpdateLightRequest } from '$lib/types';
+	import { error } from '@sveltejs/kit';
+
+	export let light: Light = {
+	  name: '',
+	  id: '',
+	  uniqueId: '',
+	  type: '',
+	  color: { xy: { x: 0, y: 0 } },
+	  on: false,
+	  dimming: {
+	    brightness: 95,
+	    minDimLevel: 0.3,
+	  },
+	};
+
+	let on = light.on;
+	let brightness = light?.dimming?.brightness;
+	let withinRepeatInterval = false;
+	const repeatIntervalMS = 150;
+
+	async function handleToggleClick() {
+	  on = !on;
+	  const req: Partial<UpdateLightRequest> = {
+	    on,
+	  };
+
+	  try {
+	    const opts: RequestInit = {
+	      method: 'PUT',
+	    };
+
+	    opts.body = JSON.stringify(req);
+	    const res = await fetch(
+	      `http://${PUBLIC_BRIDGE_IP}/api/${PUBLIC_BRIDGE_USERNAME}/lights/${light.id}/state`,
+	      opts,
+	    );
+
+	    const data = await res.json();
+	    if (!data) {
+	      throw error(500, 'No data returned from bridge');
+	    }
+	    return data;
+	  } catch (err) {
+	    throw error(500, 'Error updating light');
+	  }
+	}
+
+	async function handleBrightnessChange() {
+	  if (withinRepeatInterval) return;
+	  withinRepeatInterval = true;
+	  await updateBrightness();
+	  setTimeout(() => (withinRepeatInterval = false), repeatIntervalMS);
+	}
+
+	async function updateBrightness() {
+	  const req: Partial<UpdateLightRequest> = {
+	    bri: brightness,
+	  };
+
+	  try {
+	    const opts: RequestInit = {
+	      method: 'PUT',
+	    };
+
+	    opts.body = JSON.stringify(req);
+	    const res = await fetch(
+	      `http://${PUBLIC_BRIDGE_IP}/api/${PUBLIC_BRIDGE_USERNAME}/lights/${light.id}/state`,
+	      opts,
+	    );
+
+	    const data = await res.json();
+	    if (!data) {
+	      throw error(500, 'No data returned from bridge');
+	    }
+	    return data;
+	  } catch (err) {
+	    throw error(500, 'Error updating light');
+	  }
+	}
+	export let lightData: ILight[] = [];
+</script>
+
+<div class="overflow-x-auto">
+	<table class="table-zebra w-full">
+		<thead>
+			<tr>
+				<th>💡</th>
+				<th>id</th>
+				<th>name</th>
+				<th>type</th>
+				<th>color</th>
+				<th>brightness</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each lightData as light}
+				<LightRow {light} />
+			{/each}
+		</tbody>
+	</table>
+</div>
--- src/routes/lights/+page.svelte
+++ src/routes/lights/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Light } from '$lib/components/Light/Light';
-	import LightList from '$lib/components/LightList/LightList.svelte';
+	/* import LightList from '$lib/components/LightList/LightList.svelte'; */
+	import LightRowList from '$lib/components/LightRowList/LightRowList.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData | { lights: Light[] } = { lights: [] };
@@ -13,4 +14,4 @@
 </script>
 
 <h1 class="text-lg">Lights</h1>
-<LightList {lightData} />
+<LightRowList {lightData} />
