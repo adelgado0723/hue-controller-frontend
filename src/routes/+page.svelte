9a69b6968ba686eef049ae723123b14a74dae30c
adelgado <adelgado0723@gmail.com>
2023-03-22 20:46:14 -0400

    working on connect page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-	/* import type { PageData } from './$types'; */
-	/* export let data: PageData; */
-	/* const bgURL = new URL('../..//static/controlPanel.png', import.meta.url).href; */
+  /* import type { PageData } from './$types'; */
+  /* export let data: PageData; */
+  /* const bgURL = new URL('../..//static/controlPanel.png', import.meta.url).href; */
 </script>
 
 <div class=" hero min-h-screen" style="background-image: url('./controlPanel.png');">
-	<div class="hero-overlay bg-opacity-60" />
-	<div class="hero-content text-center text-neutral-content">
-		<div class="max-w-md text-white">
-			<h1 class="mb-5 text-5xl font-bold">Welcome to the hub...</h1>
-			<p class="mb-5">Fine grain control of hue smart home devices at your fingertips.</p>
-			<a class="btn-primary btn" href="/hub">Get Started</a>
-		</div>
-	</div>
+  <div class="hero-overlay bg-opacity-60" />
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md text-white">
+      <h1 class="mb-5 text-5xl font-bold">Welcome to the hub...</h1>
+      <p class="mb-5">Fine grain control of hue smart home devices at your fingertips.</p>
+      <a class="btn-primary btn" href="/connect">Get Started</a>
+      <a class="btn-secondary btn" href="/hub">My Hub</a>
+    </div>
+  </div>
 </div>
--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -3,21 +3,43 @@
   let currentStep = 3;
 </script>
 
-<div class="m-4 flex w-full flex-col items-center justify-center">
-  <ul class="steps m-4">
+<div class="m-50 flex w-full flex-col items-center justify-center gap-10">
+  <ul class="steps m-4 w-auto text-xl font-bold">
     <li class="{currentStep >= 1 && 'step-primary'} step font-medium">Enter Bridge's IP Addres</li>
     <li class="{currentStep >= 2 && 'step-primary'} step font-medium">Connect to the bridge</li>
     <li class="{currentStep >= 3 && 'step-primary'} step font-medium">test the conenction</li>
   </ul>
-  <div class="conect-container-enter-ip m-4">
+
+  <section
+    class="step-1 fex-col card rounded-box flex w-auto items-center justify-center gap-2 bg-base-300 p-6"
+  >
+    <!-- <div class="conect-container-enter-ip m-4"> -->
     <h1 class="text-2xl">Enter Bridge's IP Addres</h1>
-    <p class="explain">
+    <p class="explain text-xl">
       Make sure you're connected to the same network as the bridge and not using a VPN.
     </p>
-    <a class="btn-primary btn text-xl normal-case" href="https://discovery.meethue.com/"
+    <a class="btn-primary btn max-w-xs text-xl normal-case" href="https://discovery.meethue.com/"
       >Discover IP</a
     >
 
     <Input id="ip" label="Enter the bridge's IP address" type="text" required={true} />
-  </div>
+
+    <!-- </div> -->
+  </section>
+  <div class="divider" />
+  <section
+    class="step-2 fex-col card rounded-box flex w-auto items-center justify-center gap-2 bg-base-300 p-6"
+  >
+    <div class="conect-container-connect-to-bridge m-4">
+      <h1 class="text-2xl">Connect To Bridge Device</h1>
+    </div>
+  </section>
+  <div class="divider" />
+  <section
+    class="step-3 fex-col card rounded-box flex w-auto items-center justify-center gap-2 bg-base-300 p-6"
+  >
+    <div class="conect-container-connect-to-bridge m-4">
+      <h1 class="text-2xl">Confirm Connection Succeeded</h1>
+    </div>
+  </section>
 </div>
