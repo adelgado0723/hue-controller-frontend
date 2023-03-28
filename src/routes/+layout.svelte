6e405502ac5d4d883a4ebf1fd5f0e2de92a6653b
adelgado <adelgado0723@gmail.com>
2023-03-28 19:43:21 -0400

    adds carousel and connects it to steps on connect page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,61 +1,61 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import '../app.postcss';
-	import { getImageURL } from '$lib/utils';
-	export let data: PageData;
+  import type { PageData } from './$types';
+  import '../app.postcss';
+  import { getImageURL } from '$lib/utils';
+  export let data: PageData;
 </script>
 
 <div class="min-h-full">
-	<nav class="navbar bg-base-100 border-b">
-		<div class="flex-1">
-			<a class="btn btn-ghost normal-case text-3xl" href="/">Hue Hub</a>
-		</div>
+  <nav class="navbar border-b bg-base-100">
+    <div class="flex-1">
+      <a class="btn-link no-underline hover:no-underline hover:scale-110 btn text-3xl normal-case text-primary" href="/">Hue Hub</a>
+    </div>
 
-		{#if !data.user}
-			<div class="dropdown dropdown-end">
-				<a href="/login" class="btn btn-primary">Login</a>
-				<a href="/signup" class="btn btn-secondary">Register</a>
-			</div>
-		{:else}
-			<div class="dropdown dropdown-end mr-4">
-				<a href="/scenes/new" class="btn btn-primary">Add Scene</a>
-			</div>
-			<div class="dropdown dropdown-end">
-				<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-				<!-- svelte-ignore a11y-label-has-associated-control -->
-				<!-- for safari -->
-				<label tabindex="0" class="btn btn-ghost btn-circle avatar">
-					<div class="w-10 rounded-full">
-						<img
-							src={data.user?.avatar
-								? getImageURL(data.user?.collectionId, data.user?.id, data.user?.avatar)
-								: `https://ui-avatars.com/api/name=${data.user?.name}`}
-							alt="User avatar"
-						/>
-					</div>
-				</label>
-				<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-				<ul
-					tabindex="0"
-					class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-				>
-					<li>
-						<a href="/scenes" class="justify-between">My Scenes</a>
-					</li>
-					<li>
-						<a href="/lights" class="justify-between">My Lights</a>
-					</li>
-					<li>
-						<a href="/my/settings/profile">Settings</a>
-					</li>
-					<li>
-						<form action="/logout" method="POST">
-							<button type="submit" class="w-full text-start">Logout</button>
-						</form>
-					</li>
-				</ul>
-			</div>
-		{/if}
-	</nav>
-	<slot />
+    {#if !data.user}
+      <div class="dropdown-end dropdown">
+        <a href="/login" class="btn-primary btn">Login</a>
+        <a href="/signup" class="btn-secondary btn">Register</a>
+      </div>
+    {:else}
+      <div class="dropdown-end dropdown mr-4">
+        <a href="/scenes/new" class="btn-primary btn">Add Scene</a>
+      </div>
+      <div class="dropdown-end dropdown">
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <!-- for safari -->
+        <label tabindex="0" class="btn-ghost btn-circle avatar btn">
+          <div class="w-10 rounded-full">
+            <img
+              src={data.user?.avatar
+                ? getImageURL(data.user?.collectionId, data.user?.id, data.user?.avatar)
+                : `https://ui-avatars.com/api/name=${data.user?.name}`}
+              alt="User avatar"
+            />
+          </div>
+        </label>
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <ul
+          tabindex="0"
+          class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+        >
+          <li>
+            <a href="/scenes" class="justify-between">My Scenes</a>
+          </li>
+          <li>
+            <a href="/lights" class="justify-between">My Lights</a>
+          </li>
+          <li>
+            <a href="/my/settings/profile">Settings</a>
+          </li>
+          <li>
+            <form action="/logout" method="POST">
+              <button type="submit" class="w-full text-start">Logout</button>
+            </form>
+          </li>
+        </ul>
+      </div>
+    {/if}
+  </nav>
+  <slot />
 </div>
--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -1,45 +1,78 @@
 <script lang="ts">
   import Input from '$lib/components/Input/Input.svelte';
-  let currentStep = 3;
+  let currentStep = 1;
+  let content = '';
+
+  async function handleGetBridgeIPButtonClick() {
+    try {
+      const fetchRes = await fetch(`https://discovery.meethue.com/`);
+      console.log(JSON.stringify(fetchRes, null, 2));
+      const res: {id: string, internalipaddress: string, port: number} = await fetchRes?.json();
+      content = res?.internalipaddress || 'Error getting Bridge IP';
+    } catch (error) {
+      // handle error
+      console.log(error);
+      content = 'error: ' + error;
+    }
+  }
 </script>
 
 <div class="m-50 flex w-full flex-col items-center justify-center gap-10">
   <ul class="steps m-4 w-auto text-xl font-bold">
     <li class="{currentStep >= 1 && 'step-primary'} step font-medium">Enter Bridge's IP Addres</li>
     <li class="{currentStep >= 2 && 'step-primary'} step font-medium">Connect to the bridge</li>
-    <li class="{currentStep >= 3 && 'step-primary'} step font-medium">test the conenction</li>
+    <li class="{currentStep >= 3 && 'step-primary'} step font-medium">Test the connection</li>
   </ul>
+</div>
 
-  <section
-    class="step-1 fex-col card rounded-box flex w-auto items-center justify-center gap-2 bg-base-300 p-6"
-  >
-    <!-- <div class="conect-container-enter-ip m-4"> -->
-    <h1 class="text-2xl">Enter Bridge's IP Addres</h1>
-    <p class="explain text-xl">
-      Make sure you're connected to the same network as the bridge and not using a VPN.
-    </p>
-    <a class="btn-primary btn max-w-xs text-xl normal-case" href="https://discovery.meethue.com/"
-      >Discover IP</a
+<div class="carousel w-full">
+  <div id="slide1" class="carousel-item relative w-full box-border">
+    <section
+      class="step-1 fex-col card rounded-box flex w-full items-center justify-center gap-2 bg-base-300 p-12 m-5"
     >
+      <h1 class="text-2xl my-6">Enter Bridge's IP Addres</h1>
+      <p class="explain text-xl">
+        Make sure you're connected to the same network as the bridge and not using a VPN.
+      </p>
 
-    <Input id="ip" label="Enter the bridge's IP address" type="text" required={true} />
-
-    <!-- </div> -->
-  </section>
-  <div class="divider" />
-  <section
-    class="step-2 fex-col card rounded-box flex w-auto items-center justify-center gap-2 bg-base-300 p-6"
-  >
-    <div class="conect-container-connect-to-bridge m-4">
-      <h1 class="text-2xl">Connect To Bridge Device</h1>
+      <button
+        class="btn-primary btn z-10 text-2xl normal-case"
+        on:click={() => {
+          handleGetBridgeIPButtonClick();
+        }}
+      >
+      Discover IP
+      </button>
+      <div>{JSON.stringify(content, null, 2)}</div>
+      <Input id="ip" label="Enter the bridge's IP address" type="text" required={true} />
+    </section>
+    <div class="absolute left-5 right-5 top-1/2 flex justify-end">
+      <a href="#slide2" on:click={() => { currentStep = 2;}} class="btn-circle btn">❯</a>
     </div>
-  </section>
-  <div class="divider" />
-  <section
-    class="step-3 fex-col card rounded-box flex w-auto items-center justify-center gap-2 bg-base-300 p-6"
-  >
-    <div class="conect-container-connect-to-bridge m-4">
-      <h1 class="text-2xl">Confirm Connection Succeeded</h1>
+  </div>
+  <div id="slide2" class="carousel-item relative w-full">
+    <section
+      class="step-2 fex-col card rounded-box flex w-full items-center justify-center gap-2 bg-base-300 p-12 m-5"
+    >
+      <div class="conect-container-connect-to-bridge m-4">
+        <h1 class="text-2xl my-6">Connect To Bridge Device</h1>
+      </div>
+    </section>
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide1" on:click={() => { currentStep = 1;}} class="btn-circle btn">❮</a>
+      <a href="#slide3" on:click={() => { currentStep = 3;}} class="btn-circle btn">❯</a>
     </div>
-  </section>
+  </div>
+  <div id="slide3" class="carousel-item relative w-full">
+    <section
+      class="step-3 fex-col card rounded-box flex w-full items-center justify-center gap-2 bg-base-300 p-12 m-5"
+    >
+      <div class="conect-container-connect-to-bridge m-4">
+        <h1 class="text-2xl my-6">Confirm Connection Succeeded</h1>
+      </div>
+    </section>
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide2"  on:click={() => { currentStep = 2;}} class="btn-circle btn">❮</a>
+    </div>
+  </div>
 </div>
